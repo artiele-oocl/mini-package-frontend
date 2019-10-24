a641e6eba89043f850f4db2644a435560ec05f3e
artiele-oocl <artiele@oocl.com>
2019-10-24 10:20:37 +0800

    Add Cancel button to reset booking information

---
+++ src/components/Book/BookItem.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+export default class BookItem extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            inputPackageNumber: '',
+            inputPickupTime: ''
+        }
+        this.baseState = this.state;
+    }
+    dispatch = () => {
+        console.log("dispatch")
+        console.log(this.state)
+    }
+    cancelBook = (e) => {
+        e.preventDefault();
+        this.setState(this.baseState);
+    }
+    handleInputPackageNumber = (event) => {
+        this.setState({ inputPackageNumber: event.target.value});
+    };
+    handleInputPickupTime = (event) => {
+        this.setState({ inputPickupTime: event.target.value});
+    };
+    render() {
+        return(
+            <div>
+                <label>
+                    Package number:
+                    <input onChange={this.handleInputPackageNumber}
+                    value={this.state.inputPackageNumber}
+                    type="text" placeholder="Enter package number..." name="package_number" />
+                </label>
+                <label>
+                    Pickup Time:
+                    <input onChange={this.handleInputPickupTime}
+                    value={this.state.inputPickupTime}
+                    type="text" placeholder="Enter package number..." name="pickup_time" />
+                </label>
+                <button type="submit" onClick={this.dispatch}>Book</button>
+                <button type="button" onClick={this.cancelBook}>Cancel</button>
+            </div>
+        )
+    }
+}
